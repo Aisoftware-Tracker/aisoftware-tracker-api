b4cca1c167297a86fedc20d616fa006e448c5fa1
Igor Araujo <igor@moviy.com.br>
2021-09-03 00:25:56 -0300

    feat: refactor header constants

--- src/Aisoftware.Tracker.Borders/Constants/Header.cs
+++ src/Aisoftware.Tracker.Borders/Constants/Header.cs
@@ -3,13 +3,13 @@ namespace Aisoftware.Tracker.Borders.Constants
     public static class Header
     {
         public const string GEO_LOCATION = "geo-location";
-        public const string APP_ORIGIN = "X-FaturaBem-App-Origin";
-        public const string APP_VERSION = "X-FaturaBem-App-Version";
-        public const string DEVICE = "X-FaturaBem-Device";
-        public const string IP = "X-FaturaBem-IP";
-        public const string MAC_ADDRESS = "X-FaturaBem-Mac-Address";
-        public const string ORGANIZATION = "X-FaturaBem-Organization";
-        public const string OS = "X-FaturaBem-OS";
+        public const string APP_ORIGIN = "X-Aisoftware-App-Origin";
+        public const string APP_VERSION = "X-Aisoftware-App-Version";
+        public const string DEVICE = "X-Aisoftware-Device";
+        public const string IP = "X-Aisoftware-IP";
+        public const string MAC_ADDRESS = "X-Aisoftware-Mac-Address";
+        public const string ORGANIZATION = "X-Aisoftware-Organization";
+        public const string OS = "X-Aisoftware-OS";
 
     }
 }
