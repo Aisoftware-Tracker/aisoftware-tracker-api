a9021db621c65ac7e6980b9c9f262c208d2475ef
Igor Araujo <igoraujo93@gmail.com>
2021-11-02 16:37:34 -0300

    feat: fix ide problems

--- src/Aisoftware.Tracker.Api/Controllers/SessionController.cs
+++ src/Aisoftware.Tracker.Api/Controllers/SessionController.cs
@@ -7,6 +7,10 @@ using Microsoft.Extensions.Logging;
 using Aisoftware.Tracker.UseCases.Session.Interfaces;
 using Aisoftware.Tracker.Borders.Users.DTO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Net.Http;
+using System.Web;
 
 namespace Aisoftware.Tracker.Api.Controllers
 {
@@ -26,7 +30,31 @@ namespace Aisoftware.Tracker.Api.Controllers
         {
             var response = await _useCase.CreateSession(request);
 
+            var cookieOptions = new CookieOptions
+            {
+                Secure = false,
+                HttpOnly = false,
+                SameSite = SameSiteMode.None
+            };
+
+            // Add the cookie to the response cookie collection
+            Response.Cookies.Append("JSESSIONID", response.Cookie.Value, cookieOptions);
+
             return Ok(response);
         }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Find()
+        {
+
+            string cookie = Request.Cookies["JSESSIONID"];
+
+            Console.WriteLine("Passou na controller");
+            var response = await _useCase.Find(cookie);
+
+            return Ok(response);
+        }
+
     }
 }
